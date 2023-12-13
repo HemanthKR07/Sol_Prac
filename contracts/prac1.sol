8be341bdc651480e1aefc536e3c25181ed9761bf
Hemanth KR <krh7799@gmail.com>
2023-12-13 23:59:45 +0530

    SetupDone

--- contracts/prac1.sol
+++ contracts/prac1.sol
@@ -1,16 +1,19 @@
 pragma solidity ^0.8.0;
 
 contract Practice {
+    string public name;
+    uint public age;
+
     constructor() {
-        name;
-        age;
+        name = "Hello";
+        age = 18;
     }
 
-    function getName() public returns (string memory) {
+    function getName() public view returns (string memory) {
         return name;
     }
 
-    function getAge() public returns (uint) {
+    function getAge() public view returns (uint) {
         return age;
     }
 }
