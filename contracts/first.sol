e2c07d8753bdb8c45ccf035c50afa67de5a5fa1a
Hemanth KR <krh7799@gmail.com>
2023-12-14 16:01:55 +0530

    NotAbleToDeploy

--- contracts/first.sol
+++ contracts/first.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 contract first {
     string public name = "Rohan";
     uint public age = 18;
 
-    function getName() public view returns (string memory) {
-        return name;
-    }
+    // function getName() public view returns (string memory) {
+    //     return name;
+    // }
 
-    function getAge() public view returns (uint) {
-        return age;
-    }
+    // function getAge() public view returns (uint) {
+    //     return age;
+    // }
 
     // function setAge() public {
     //     age += 2;
